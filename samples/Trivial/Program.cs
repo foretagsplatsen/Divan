ab52c2fe3121a17bccefb0b9e73bcc47f6dc6eb4
Foretagsplatsen <info@foretagsplatsen.se>
2009-08-27 16:54:24 +0200

    Improved the Trival example showing some more stuff.

--- samples/Trivial/Program.cs
+++ samples/Trivial/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Divan;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -7,12 +9,13 @@ using Newtonsoft.Json.Linq;
 namespace Trivial
 {
     /// <summary>
-    /// A trivial example of using Divan. Requires a running CouchDB on localhost!
+    /// A trivial example of using Divan. Requires a running CouchDB on localhost or some other server.
     /// 
     /// Run using:
     ///
     ///     Trivial.exe <host> <port>
     ///
+    /// If you leave out arguments it will use localhost:5984.
     /// </summary>
     class Program
     {
@@ -39,22 +42,32 @@ namespace Trivial
                     break;
             }
 
-            // Get a server for default couch port 5984 on localhost
+            // Get a server instance. It only holds host, port and a string database prefix.
+            // For non trivial usage of Divan you typically create your own subclass of CouchServer.
             var server = new CouchServer(host, port);
-            Console.WriteLine("Created a CouchServer");
 
-            // Get (creates it if needed) a CouchDB database.
+            // Get (creates it if needed) a CouchDB database. This call will create the db in CouchDB
+            // if it does not exist, create a CouchDatabase instance and then send Initialize() to it
+            // before returning it. The base class CouchDatabase also has very little state, it knows
+            // only the server that it belongs to and its own name.
             var db = server.GetDatabase("trivial");
             Console.WriteLine("Created database 'trivial'");
 
-            // Create and save 10 Cars with automatically allocated Ids by Couch
+            // Create and save 10 Cars with automatically allocated Ids by CouchDB.
+            // Divan stores ICouchDocuments and there are several ways you can go:
+            //   - Create a subclass of CouchDocument (like Car is here).
+            //   - Create a class that implements ICouchDocument.
+            //   - Create a subclass of CouchJsonDocument.
             Car car = null;
             for (int i = 0; i < 10; i++)
             {
-                car = new Car("Saab", "93", 170);
+                car = new Car("Saab", "93", 170+i);
                 db.SaveDocument(car);
             }
-            Console.WriteLine("Saved 10 Cars with 170 hps each.");
+            Console.WriteLine("Saved 10 Cars with 170-180 hps each.");
+
+            // Load all Cars as CouchJsonDocuments and print one of them
+            Console.WriteLine("Here is the first of the Cars: \n\n" + db.GetAllDocuments().First().ToString());
 
             // Modify the last Car we saved...
             car.HorsePowers = 400;
@@ -68,19 +81,29 @@ namespace Trivial
             var sameCar = db.GetDocument<Car>(car.Id);
             Console.WriteLine("Loaded last Car " + sameCar.Make + " " + sameCar.Model + " now with " + sameCar.HorsePowers + "hps.");
 
-            // Load all Cars. QueryAllDocuments() gives us a CouchQuery which we can configure. We tell it to IncludeDocuments()
+            // Load all Cars. If we dwelve into the GetAllDocuments() method we will see that
+            // QueryAllDocuments() gives us a CouchQuery which we can configure. We tell it to IncludeDocuments()
             // which means that we will get back not only ids but the actual documents too. GetResult() will perform the
             // HTTP request to CouchDB and return a CouchGenericViewResult which we in turn can ask to produce objects from JSON,
             // in this case we pick out the actual documents and instantiate them as instances of the class Car.
-            var cars = db.QueryAllDocuments().IncludeDocuments().GetResult().Documents<Car>();
+            var cars = db.GetAllDocuments<Car>();
             Console.WriteLine("Loaded all Cars: " + cars.Count);
 
-            // Delete all Cars one by one
-            foreach (var eachCar in cars)
+            // Delete some Cars one by one. CouchDB is an MVCC database which means that for every operation that modifies a document
+            // we need to supply not only its document id, but also the revision that we are aware of. This means that we must supply id/rev
+            // for each document we want to delete.
+            foreach (var eachCar in cars.Where(x => x.HorsePowers > 175))
             {
                 db.DeleteDocument(eachCar);
                 Console.WriteLine("Deleted car with id " + eachCar.Id);
             }
+
+            // Get all cars again and see how many are left.
+            Console.WriteLine("Cars left: " + db.GetAllDocuments<Car>().Count);
+
+            //  We can actually also delete in a single bulk call using DeleteDocuments().
+            db.DeleteDocuments(cars.ToArray());
+            Console.WriteLine("Deleted the rest of the cars");
 
             // Delete the db itself
             db.Delete();
