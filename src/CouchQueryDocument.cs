bbafde3a4e24deea1d2ef17aa13324e3927c5e22
Martin Murphy <martin.murphy@whiteboard-it.com>
2010-05-10 10:01:15 +0800

    BugFix
        When null was emitted for the value it was failing.
    New behavior assigns null to Rev when _rev cannot be found in the value emitted.

--- src/CouchQueryDocument.cs
+++ src/CouchQueryDocument.cs
@@ -13,7 +13,8 @@ namespace Divan
         {
             Id = obj["id"].Value<string>();
             Key = obj["key"].Value<string>();
-            Rev = (obj["value"].Value<JObject>())["rev"].Value<string>();
+        	var tmp = obj["value"];
+			Rev = tmp.ToString() == "null" ? null : tmp.Value<JObject>()["_rev"].Value<string>(); //Rev is null if the value emitted is not doc or does not contain _rev
         }
     }
 }
