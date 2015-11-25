e842faae229ed12dfc1af75245ead96e04f5955b
Vladyslav Chyzhevskyi <vchyzhevskyi@pgs-soft.com>
2015-11-25 12:41:44 +0100

    On CouchDB v1.6.1 request: GET /{db}/_all_docs returns "rev" property instead of "_rev" in previous versions

--- src/CouchQueryDocument.cs
+++ src/CouchQueryDocument.cs
@@ -14,7 +14,7 @@ namespace Divan
             Id = obj["id"].Value<string>();
             Key = obj["key"].Value<string>();
         	var tmp = obj["value"];
-			Rev = tmp.ToString() == "null" ? null : tmp.Value<JObject>()["_rev"].Value<string>(); //Rev is null if the value emitted is not doc or does not contain _rev
+			Rev = tmp.ToString() == "null" ? null : tmp.Value<JObject>()["rev"].Value<string>(); //Rev is null if the value emitted is not doc or does not contain _rev
         }
     }
 }
